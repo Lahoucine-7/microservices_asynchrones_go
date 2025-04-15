f8dbe8d9fc86310352cd087bdbb4cd0c03a4bc12
Lahoucine <lahoucine.elfarra@gmail.com>
2025-04-15 04:36:33 +0200

    Architecture du projet

---
+++ service-notifications/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Service Notifications lancé avec succès 🚀")
+}
---
+++ service-utilisateurs/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Service Utilisateurs lancé avec succès 🚀")
+}
